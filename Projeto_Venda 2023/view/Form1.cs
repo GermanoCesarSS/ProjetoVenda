f6a04c60f141f36ba0b110ad0685dec0c0ea2d1d
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2023-11-10 20:29:27 -0300

    EXEMPLO VIEW

---
+++ Projeto_Venda 2023/view/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Projeto_Venda_2023.view
+{
+    public partial class Form1 : Projeto_Venda_2023.frmBase
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
